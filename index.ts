cebf28af9305b04c8683e5fd49eeb2e19ffbd7b5
Akshat Sipany <akshatsipany2106@gmail.com>
2026-05-16 08:52:44 +0530

    addded express app

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import express from "express"
+
+const app = express();
+
+app.get("/",(req,res) => {
+	res.send("Hello");
+});
+
+app.listen(3000,() => {
+	console.log("Server up at port 3000");
+});
